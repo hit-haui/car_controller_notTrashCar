b31657920a939845b49e2c89ef43844ae4f81250
ducvietmta <ducviet.mta@gmail.com>
2018-12-29 19:11:14 +0700

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,6 @@
 #include "api_i2c_pwm.h"
 PCA9685 *pca9685 = new PCA9685() ;
 bool STATUS = 0;
-int dir = 0, throttle_val = 10;
-char key = 0;
-int getkey() {
-	int character; 
-	struct termios orig_term_attr;
-	struct termios new_term_attr;
-
-	/* set the terminal to raw mode */
-	tcgetattr(fileno(stdin), &orig_term_attr);
-	memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
-	new_term_attr.c_lflag &= ~(ECHO|ICANON);
-	new_term_attr.c_cc[VTIME] = 0;
-	new_term_attr.c_cc[VMIN] = 0;
-	tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
-
-	/* read a character from the stdin stream without blocking */
-	/*   returns EOF (-1) if no character is available */
-	character = fgetc(stdin);
-
-	/* restore the original terminal attributes */
-	tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
-
-	return character;
-}
 
 void speed(const std_msgs::Float32::ConstPtr& msg){
 	if (!STATUS) return;
@@ -50,37 +26,18 @@ void steer(const std_msgs::Float32::ConstPtr& msg2){
 	api_set_STEERING_control( pca9685,steer);
 	ROS_INFO("new steer: [%f]",steer);
 }
-void status(const std_msgs::Bool::ConstPtr& msg)
-{
-	STATUS = msg->data;
-	if (!STATUS)
-	{
-		double speed = 0;
-		double steer = 0;
-		api_set_FORWARD_control(pca9685, speed);
-		api_set_STEERING_control( pca9685, steer);
-	}
-}
+
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "servo_controller");
+	ros::init(argc, argv, "car_controller");
 	api_pwm_pca9685_init( pca9685 );
-
 	double spd = 0;
 	api_set_FORWARD_control(pca9685, spd);
-
 	ros::NodeHandle n;
 	ros::NodeHandle m;
-	ros::NodeHandle p;
-	ros::Subscriber sub1 = n.subscribe("/speed", 1000, speed);
-	ros::Subscriber sub2 = m.subscribe("/steerAngle", 1000, steer);		
-	ros::Subscriber sub3 = p.subscribe("/status", 1000, status);
-
-	/**
-	 * Spin dùng để loop các message
-	 */
+	ros::Subscriber sub1 = n.subscribe("/speed_api", 10, speed);
+	ros::Subscriber sub2 = m.subscribe("/steer_api", 10, steer);		
 	ros::spin();
-
 	return 0;
 
 }
